10f440ec3f6c646c7b8b07378d760621b62d3ef6
Sam Brannon <sam@sambrannon.com>
2019-11-18 18:44:21 -0600

    nextjs version

---
+++ pages/support.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Page from '../src/components/page';
+import ContentWrapper from '../src/components/contentWrapper';
+
+const Support = () => (
+  <Page title="Support | OldDead">
+    <ContentWrapper>
+      <h1>Support</h1>
+      <p>For support, general inquiries and other help, please email us at olddead@branberg.com</p>
+    </ContentWrapper>
+  </Page>
+);
+
+export default Support;
