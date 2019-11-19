58a8c5984b353c1924ed831933cb6fc998e441ea
Sam Brannon <sam@reverb.com>
2019-11-18 18:44:21 -0600

    nextjs version

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Page from '../src/components/page';
+
+const Home = () => (
+  <Page title="OldDead">
+    <img src="/images/app-icon.svg" alt="OldDead" className="icon" />
+  </Page>
+);
+
+export default Home;
